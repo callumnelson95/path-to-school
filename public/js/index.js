6d84c13751148e62f1df3c0f8c6ff4d8da82148f
callumnelson95 <callum_nelson@brown.edu>
2018-03-16 12:21:24 -0400

    Set up database, added some data, created three views, working on js for page1

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+ 	run();
+});
+
+
+
+function run(){
+	$( "#data_grabber" ).click(function() {
+  		alert( "Handler for .click() called." );
+	});
+}
--- server.js
+++ server.js
@@ -2,20 +2,49 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var engines = require('consolidate');
 var path = require('path');
-var pug = require('pug');
+var mysql = require('mysql');
 var app = express();
  
 // set up the template engine
+app.engine('html', engines.hogan);
 app.set('views', './views');
-app.set('view engine', 'pug');
+app.set('view engine', 'html');
  
 // GET response for '/'
 app.get('/', function (req, res) {
     // render the 'index' template, and pass in a few variables
-    res.render('index.pug');
-    
+    res.render('index.html');
+
 });
- 
+
+app.get('/page1.html', function(request, response){
+	console.log('page1 requested');
+	response.render('page1.html');
+});
+
+app.get('/page2.html', function(request, response){
+	console.log('page2 requested');
+	response.render('page2.html');
+});
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Brownbear1!",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("SELECT * FROM basic_info", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
 // start up the server
 app.listen(8080, function () {
     console.log('Listening on http://localhost:8080');
