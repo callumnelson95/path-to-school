fdf4fd8da430bbe3f5e0099829b36a8d42313b62
callumnelson95 <callum_nelson@brown.edu>
2018-03-16 10:10:07 -0400

    adding packages

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var engines = require('consolidate');
+var path = require('path');
+var pug = require('pug');
+var app = express();
+ 
+// set up the template engine
+app.set('views', './views');
+app.set('view engine', 'pug');
+ 
+// GET response for '/'
+app.get('/', function (req, res) {
+    // render the 'index' template, and pass in a few variables
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
+ 
+});
+ 
+// start up the server
+app.listen(8080, function () {
+    console.log('Listening on http://localhost:3000');
+});
+
